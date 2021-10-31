4ca8b3ceb1a923aefb73007c4532f858b55b484a
Priyasettu27 <93431628+Priyasettu27@users.noreply.github.com>
2021-10-31 19:01:12 +0530

    Add files via upload

---
+++ MyCaluclator.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class MyCaluclator {
+
+	public static void main(String[] args) {
+		Calculator Calc=new Calculator();
+		System.out.println(Calc.add(10,20,30));
+		System.out.println(Calc.sub(30,20));
+		System.out.println(Calc.mul(233.34,101));
+		System.out.println(Calc.div(2, 10));
+
+	}
+
+}
